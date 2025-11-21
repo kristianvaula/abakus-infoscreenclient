28d0470b4e438a14371deadf8d76cbe808ae0c8f
kristianvaula <kristvje@stud.ntnu.no>
2025-11-21 16:49:58 +0100

    Image test

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,14 +1,13 @@
-'use client';
-
 export default function InfoBox() {
-
   return (
     <div className="info-box">
-      <h1 className="">Velkommen til Abakus</h1>
-      <p>
-        Abakus er linjeforeningen for studentene ved <em>Datateknologi & Cybersikkerhet og datakommunikasjon</em> på NTNU, og drives av studenter ved disse studiene.
-      </p>
-      <br></br>
+      <div className="info-content">
+        <h1>Velkommen til Abakus</h1>
+        <p>
+          Abakus er linjeforeningen for studentene ved <em>Datateknologi & Cybersikkerhet og datakommunikasjon</em> på NTNU, og drives av studenter ved disse studiene.
+        </p>
+      </div>
     </div>
   );
 }
+
